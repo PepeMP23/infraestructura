23ea9a9333afc940203f472e20222b9a7f46e6a9
Jose Muñeton Perez <josemunetonperez@192.168.1.204>
2025-10-16 21:49:49 -0600

    Proyecto infraestructura listo

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,20 @@
-// server.js
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
 // Middleware
-app.use(cors()); // permite cualquier origen
+app.use(cors());
 app.use(express.json());
 
 // Rutas
 app.get("/api/mensaje", (req, res) => {
-  res.json({ mensaje: "Hola desde el backend (GET) 🚀" });
+  res.json({ mensaje: "Hola desde el backend 🚀" });
 });
 
 app.post("/api/datos", (req, res) => {
-  const data = req.body;
-  res.json({ recibido: true, data });
+  res.json({ recibido: true, data: req.body });
 });
 
-// Servidor
+// Puerto dinámico (Render)
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`✅ Servidor en puerto ${PORT}`));
